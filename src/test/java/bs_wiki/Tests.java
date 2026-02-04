1d5ec4d22655a8cbfee1e49463e9ae3684bf298f
alexaastri <AVoronin@lanit.ru>
2026-02-04 15:25:41 +0300

    debug

--- src/test/java/bs_wiki/Tests.java
+++ src/test/java/bs_wiki/Tests.java
@@ -1,22 +1,11 @@
 package bs_wiki;
 
-import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.Screenshots;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
-import helpers.Attach;
-import io.appium.java_client.AppiumBy;
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
 import io.qameta.allure.Feature;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Dimension;
 import wikipages.MainScreen;
-
-import java.util.Set;
 
 import static com.codeborne.selenide.Selenide.*;
 
@@ -29,7 +18,7 @@ public class Tests extends BaseBrowserstackTest {
     @Feature("Search line")
     @DisplayName("Check the result isn't empty")
     void searchEmptyTest() {
-        //back();
+
         boolean actualResult = mainScreen
                 .clickOnFakeSearchField()
                 .sendSearchPhraseInRealSearchField("NASCAR")
@@ -38,95 +27,95 @@ public class Tests extends BaseBrowserstackTest {
         Assertions.assertTrue(actualResult);
     }
 
-//    @Test
-//    @Feature("Tabs")
-//    @DisplayName("Check the header of the tab 'saved' ")
-//    void savedTabTest() {
-//        back();
-//        String expectedHeader = mainScreen
-//                .clickOnSavedTab()
-//                .getSavedTabHeader();
-//
-//        Assertions.assertEquals("Saved", expectedHeader);
-//    }
-//
-//    @Test
-//    @Feature("Tabs")
-//    @DisplayName("Check the header of the tab 'settings' ")
-//    void savedSettingsTest() {
-//        back();
-//        String expectedHeader = mainScreen
-//                .clickOnMoreTab()
-//                .clickOnSettings()
-//                .getSettingsTabHeader();
-//
-//        Assertions.assertEquals("Settings", expectedHeader);
-//    }
-//
-//    @Test
-//    @Feature("Settings switches")
-//    @DisplayName("Check switch 'show link previews' ")
-//    void showPreviewTest() {
-//        back();
-//        boolean expectedShowPreviewCondition = mainScreen
-//                .clickOnMoreTab()
-//                .clickOnSettings()
-//                .checkShowPreviewIsTrue();
-//
-//        Assertions.assertTrue(expectedShowPreviewCondition);
-//    }
-//
-//    @Test
-//    @Feature("Settings switches")
-//    @DisplayName("Check switch 'prefer offline' ")
-//    void preferOfflineTest() {
-//        back();
-//        boolean expectedPreferOfflineCondition = mainScreen
-//                .clickOnMoreTab()
-//                .clickOnSettings()
-//                .scrollToPreferOffline()
-//                .checkPreferOfflineIsFalse();
-//
-//        Assertions.assertTrue(expectedPreferOfflineCondition);
-//    }
-//
-//    @Test
-//    @Feature("Tabs")
-//    @DisplayName("Check the header of the tab 'activity' ")
-//    void activityTest() {
-//        back();
-//        String expectedHeader = mainScreen
-//                .clickOnActivityTab()
-//                .getActivityHeader();
-//
-//        Assertions.assertEquals("Activity",expectedHeader);
-//    }
-//
-//    @Test
-//    @Feature("Tabs")
-//    @DisplayName("Check the header on activity custom screen")
-//    void activityCustomHeaderTest() {
-//        back();
-//        String expectedHeader = mainScreen
-//                .clickOnActivityTab()
-//                .goToInnerActivityTab()
-//                .clickOnCustomizeOption()
-//                .getCustomizeHeaderText();
-//
-//        Assertions.assertEquals("Customize",expectedHeader);
-//    }
-//
-//    @Test
-//    @Feature("Tabs")
-//    @DisplayName("Check the switches on activity custom screen")
-//    void activityCustomSwitchesTest() {
-//        back();
-//        boolean allSwitchesAreShown = mainScreen
-//                .clickOnActivityTab()
-//                .goToInnerActivityTab()
-//                .clickOnCustomizeOption()
-//                .areAllSwitchTextsCorrect();
-//
-//        Assertions.assertTrue(allSwitchesAreShown);
-//    }
+    @Test
+    @Feature("Tabs")
+    @DisplayName("Check the header of the tab 'saved' ")
+    void savedTabTest() {
+
+        String expectedHeader = mainScreen
+                .clickOnSavedTab()
+                .getSavedTabHeader();
+
+        Assertions.assertEquals("Saved", expectedHeader);
+    }
+
+    @Test
+    @Feature("Tabs")
+    @DisplayName("Check the header of the tab 'settings' ")
+    void savedSettingsTest() {
+
+        String expectedHeader = mainScreen
+                .clickOnMoreTab()
+                .clickOnSettings()
+                .getSettingsTabHeader();
+
+        Assertions.assertEquals("Settings", expectedHeader);
+    }
+
+    @Test
+    @Feature("Settings switches")
+    @DisplayName("Check switch 'show link previews' ")
+    void showPreviewTest() {
+
+        boolean expectedShowPreviewCondition = mainScreen
+                .clickOnMoreTab()
+                .clickOnSettings()
+                .checkShowPreviewIsTrue();
+
+        Assertions.assertTrue(expectedShowPreviewCondition);
+    }
+
+    @Test
+    @Feature("Settings switches")
+    @DisplayName("Check switch 'prefer offline' ")
+    void preferOfflineTest() {
+
+        boolean expectedPreferOfflineCondition = mainScreen
+                .clickOnMoreTab()
+                .clickOnSettings()
+                .scrollToPreferOffline()
+                .checkPreferOfflineIsFalse();
+
+        Assertions.assertTrue(expectedPreferOfflineCondition);
+    }
+
+    @Test
+    @Feature("Tabs")
+    @DisplayName("Check the header of the tab 'activity' ")
+    void activityTest() {
+
+        String expectedHeader = mainScreen
+                .clickOnActivityTab()
+                .getActivityHeader();
+
+        Assertions.assertEquals("Activity",expectedHeader);
+    }
+
+    @Test
+    @Feature("Tabs")
+    @DisplayName("Check the header on activity custom screen")
+    void activityCustomHeaderTest() {
+
+        String expectedHeader = mainScreen
+                .clickOnActivityTab()
+                .goToInnerActivityTab()
+                .clickOnCustomizeOption()
+                .getCustomizeHeaderText();
+
+        Assertions.assertEquals("Customize",expectedHeader);
+    }
+
+    @Test
+    @Feature("Tabs")
+    @DisplayName("Check the switches on activity custom screen")
+    void activityCustomSwitchesTest() {
+
+        boolean allSwitchesAreShown = mainScreen
+                .clickOnActivityTab()
+                .goToInnerActivityTab()
+                .clickOnCustomizeOption()
+                .areAllSwitchTextsCorrect();
+
+        Assertions.assertTrue(allSwitchesAreShown);
+    }
 }
